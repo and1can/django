39b6e6874fdb45f00f937e235cf78df787e285b0
= <chu.andy1993@gmail.com>
2016-07-04 18:08:57 -0700

    Modified templates to display ports from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post # import to allow posts in database to display
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'django/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'django/post_list.html', {'posts': posts}) #posts is a variable that can 
+																	  #be used in templates
