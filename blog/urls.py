821caef2483c4c78d224f758adb9fd17f4e9b9c6
= <chu.andy1993@gmail.com>
2016-07-04 18:46:24 -0700

    Added view and template for detailed blog post as well as css for site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'), #tells django that views.post_list is place to visit
 												   #when someone enters site at http://127.0.0.1:8000/
+
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post # import to allow posts in database to display
 from django.utils import timezone
+
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'django/post_list.html', {'posts': posts}) #posts is a variable that can 
 																	  #be used in templates
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'django/post_detail.html', {'post': post})
